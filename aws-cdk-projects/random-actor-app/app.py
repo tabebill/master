cf2b7cb525404dc9bed678cd2bd2cd183e15670f
Bill <tabebill09@gmail.com>
2023-06-08 03:45:31 +0100

    commit

---
+++ aws-cdk-projects/random-actor-app/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+actors = [
+    "Tom Hanks",
+    "Meryl Streep",
+    "Leonardo DiCaprio",
+    "Jennifer Lawrence",
+    "Brad Pitt",
+    "Natalie Portman",
+    "Denzel Washington",
+    "Charlize Theron",
+    "Robert Downey Jr.",
+    "Cate Blanchett"
+]
+
+colors = [
+    "red",
+    "blue",
+    "green",
+    "orange",
+    "purple",
+    "yellow",
+    "pink",
+    "teal"
+]
+
+@app.route('/')
+def generate_actor():
+    actor = random.choice(actors)
+    color = random.choice(colors)
+    return render_template('index.html', actor=actor, color=color)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)
